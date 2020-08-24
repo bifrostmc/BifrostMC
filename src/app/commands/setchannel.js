a914cc2c87cc41f620c3acefcc991f892c51f14b
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-08-23 21:23:32 -0300

    Create list channels in setchannel

--- src/app/commands/setchannel.js
+++ src/app/commands/setchannel.js
@@ -1,3 +1,5 @@
+import { MessageEmbed } from 'discord.js';
+
 import knex from '../database';
 import configuration from '../../../configure';
 
@@ -11,7 +13,7 @@ class SetChannel {
 			requiredPermissions: ['MANAGE_CHANNELS'],
 		};
 
-		this.run = async ({ msg, args, prefix }) => {
+		this.run = async ({ msg, args, prefix, bot }) => {
 			const { channel } = msg;
 
 			async function addChannelInDatabase(function_name, name) {
@@ -60,40 +62,75 @@ class SetChannel {
 				}
 			}
 
+			const channels = {
+				banned: {
+					formatted: 'banimentos',
+					help:
+						'Esse chat recebe as atualizações de logs de status de banimento de um usuário.',
+				},
+				sugestoes: {
+					formatted: 'sugestões',
+					help:
+						'Você pode setar esse canal para receber as sugestões dos usuários.',
+				},
+				rebaixamentos: {
+					formatted: 'rebaixamentos',
+					help:
+						'Esse chat recebe as atualizações de logs de demote a um usuário.',
+				},
+				promocoes: {
+					name: 'promocoes',
+					formatted: 'promoções',
+					help:
+						'Esse chat recebe as atualizações de logs de promote a um usuário.',
+				},
+				denuncias: {
+					formatted: 'denúncias',
+					help:
+						'Esse chat recebe as logs de denúncias que os usuários relatam.',
+				},
+			};
+
 			if (args[0]) {
-				switch (args[0]) {
-					case 'banned':
-						addChannelInDatabase('banned', 'banimentos');
-						break;
-					case 'sugestoes':
-						addChannelInDatabase('sugestoes', 'sugestões');
-						break;
-					case 'rebaixamentos':
-						addChannelInDatabase('rebaixamentos', 'rebaixamentos');
-						break;
-					case 'promocoes':
-						addChannelInDatabase('promocoes', 'promoções');
-						break;
-					case 'denuncias':
-						addChannelInDatabase('denuncias', 'denúncias');
-						break;
-					default:
-						channel.send(`Nenhuma sincronização de canal feita com esse nome.`);
-						channel.send(
-							`Para saber os tipos de canais digite ${prefix}setchannel list`
+				if (args[0] === 'list') {
+					const listChannels = new Map(Object.entries(channels));
+					const embedChannels = new MessageEmbed()
+						.setTitle('**Lista dos meus canaís disponíveis**')
+						.setThumbnail(bot.user.avatarURL())
+						.setTimestamp()
+						.setFooter(
+							`Copyright © 2020 ${bot.user.username}`,
+							bot.user.avatarURL()
 						);
-						break;
+					embedChannels.addField(`\u200B`, `\u200B`, false);
+					listChannels.forEach(({ help }, name) => {
+						embedChannels.addField(
+							`\u200B`,
+							`\`${prefix}setchannel ${name}\` » \`\`\`yaml${help}\`\`\``,
+							false
+						);
+					}, listChannels);
+					msg.channel.send(embedChannels);
+					return 'O usuário veio ver a lista de canais disponíveis.';
 				}
-			} else {
-				channel
-					.send(
-						`Sintaxe incorreta, porfavor use dessa forma ${prefix}setchannel {tipo de canal}`
-					)
-					.then((msg) => msg.delete({ timeout: 5000 }));
-				channel
-					.send(`Para saber os tipos de canais digite ${prefix}setchannel list`)
-					.then((msg) => msg.delete({ timeout: 5000 }));
+				if (channels[args[0]]) {
+					addChannelInDatabase(args[0], channels[args[0]].formatted);
+
+					return `O usuário executou comando para setar o canal ${args[0]} em ${msg.channel.id}`;
+				}
+
+				channel.send(`Nenhuma sincronização de canal feita com esse nome.`);
+				channel.send(
+					`Para saber os tipos de canais digite ${prefix}setchannel list`
+				);
+				return `O usuário executou o comando porém digitou um tipo de canal inválido.`;
 			}
+			channel
+				.send(
+					`Sintaxe incorreta, porfavor use dessa forma ${prefix}setchannel {tipo de canal}\nPara saber os tipos de canais digite ${prefix}setchannel list`
+				)
+				.then((msgSyntaxError) => msgSyntaxError.delete({ timeout: 5000 }));
+			return 'O usuário digitou o comando em uma syntax incorreta';
 		};
 	}
 }
