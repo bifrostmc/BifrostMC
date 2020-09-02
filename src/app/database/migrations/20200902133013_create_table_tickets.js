9adcb76bb6492527c2e62547a1f7a62dd19f55a8
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-09-02 18:22:53 -0300

    Change apply in tickets table in database

--- src/app/database/migrations/20200902133013_create_table_tickets.js
+++ src/app/database/migrations/20200902133013_create_table_tickets.js
@@ -1,10 +1,10 @@
 exports.up = (knex) =>
 	knex.schema.createTable('tickets', (table) => {
 		table.increments('id');
-		table.string('guild_id', 25);
-		table.string('user_id', 25);
-		table.string('name_channel', 25);
-		table.string('channel_id', 25);
+		table.string('guild_id', 42);
+		table.string('user_id', 42);
+		table.string('name_channel', 42);
+		table.string('channel_id', 42);
 
 		table.timestamp('created_at').defaultTo(knex.fn.now());
 	});
