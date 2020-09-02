964220dc0dd81ee8f3b2fd6380fee6d0fa5909b2
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-09-02 18:31:35 -0300

    Trying fix of bug in delete ticket

--- src/app/controllers/TicketsController.js
+++ src/app/controllers/TicketsController.js
@@ -1,119 +1,113 @@
 const knex = require('../database/');
 
 class TicketController {
-  constructor() {
-    this.index = (req) => {
-      return new Promise((res, rej) => {
-        const { user_id } = req.body;
+	constructor() {
+		this.index = (req) => {
+			return new Promise((res, rej) => {
+				const { user_id } = req.body;
 
-        knex('tickets').where({ user_id }).limit(1).first()
-          .then((response) => {
-            res(response);
-          })
-          .catch((err) => {
-            rej(err);
-          });
-      });
-    };
+				knex('tickets').where({ user_id }).limit(1).first()
+					.then((response) => {
+						res(response);
+					})
+					.catch((err) => {
+						rej(err);
+					});
+			});
+		};
 
-    this.store = (req) => {
-      return new Promise((res, rej) => {
-        const { msg } = req.body;
-        const positions = [];
-        msg.guild.members.cache.map((user) => {
-          if (user.hasPermission('MANAGE_MESSAGES')) {
-            return positions.push({
-              id: user.user.id,
-              allow: ['VIEW_CHANNEL'],
-            });
-          }
-          return false;
-        });
-        msg.guild.channels
-          .create(`ticket-${msg.author.id}`, {
-            type: 'text',
-            reason: 'New channel added for fun!',
-            permissionOverwrites: [
-              ...positions,
-              {
-                id: msg.guild.id,
-                deny: ['VIEW_CHANNEL'],
-              },
-              {
-                id: msg.member.id,
-                allow: ['VIEW_CHANNEL'],
-              },
-            ],
-          })
-          .then((channel) => {
-            knex('tickets').insert([{
-              user_id: msg.author.id,
-              name_channel: `ticket-${msg.author.id}`,
-              channel_id: channel.id,
-              guild_id: channel.guild.id
-            }])
-              .then(() => {
-                return res(
-                  `Seu ticket foi criado clique aqui para acessa-lo: ${channel}`
-                );
-              })
-              .catch(() => {
-                channel.delete().catch(() => {});
-                return rej(
-                  new Error(
-                    'Houve um erro na requisição de criação de um ticket, tente novamente mais tarde.'
-                  )
-                );
-              });
-          })
-          .catch(() => {
-            rej(
-              new Error(
-                'Houve um erro na requisição de criação de um ticket, tente novamente mais tarde.'
-              )
-            );
-          });
-      });
-    };
+		this.store = (req) => {
+			return new Promise((res, rej) => {
+				const { msg } = req.body;
+				const positions = [];
+				msg.guild.members.cache.map((user) => {
+					if (user.hasPermission('MANAGE_MESSAGES')) {
+						return positions.push({
+							id: user.user.id,
+							allow: ['VIEW_CHANNEL'],
+						});
+					}
+					return false;
+				});
+				msg.guild.channels
+					.create(`ticket-${msg.author.id}`, {
+						type: 'text',
+						reason: 'New channel added for fun!',
+						permissionOverwrites: [
+							...positions,
+							{
+								id: msg.guild.id,
+								deny: ['VIEW_CHANNEL'],
+							},
+							{
+								id: msg.member.id,
+								allow: ['VIEW_CHANNEL'],
+							},
+						],
+					})
+					.then((channel) => {
+						knex('tickets').insert([{
+							user_id: msg.author.id,
+							name_channel: `ticket-${msg.author.id}`,
+							channel_id: channel.id,
+							guild_id: channel.guild.id
+						}])
+							.then(() => {
+								return res(
+									`Seu ticket foi criado clique aqui para acessa-lo: ${channel}`
+								);
+							})
+							.catch(() => {
+								channel.delete().catch(() => {});
+								return rej(
+									new Error(
+										'Houve um erro na requisição de criação de um ticket, tente novamente mais tarde.'
+									)
+								);
+							});
+					})
+					.catch(() => {
+						rej(
+							new Error(
+								'Houve um erro na requisição de criação de um ticket, tente novamente mais tarde.'
+							)
+						);
+					});
+			});
+		};
 
-    this.delete = (req) => {
-      return new Promise((res, rej) => {
-        const { user_id, bot, user } = req.body;
-        const sucess = `O ticket de ${user} foi deletado com sucesso!`;
-        this.verifyExists(user_id).then((result) => {
-          if (!result.state)
-            return rej(new Error(`${user} não possui um ticket.`));
-          this.eliminate(user_id, bot, result.data).then((response) => {
-            if (!response)
-              return rej(
-                new Error(`Não foi possível deletar o ticket de ${user}`)
-              );
+		this.delete = (req) => {
+			return new Promise((res, rej) => {
+				const { user_id, bot, user } = req.body;
+				const sucess = `O ticket de ${user} foi deletado com sucesso!`;
 
-            return res(sucess);
-          });
-          return res(sucess);
-        });
-      });
-    };
-  }
+				const ticket = await knex('tickets').where({ user_id }).limit(1).first();
+				console.log(ticket)
+				if (!ticket) return rej(new Error(`${user} não possui um ticket.`));
 
-  async verifyExists(user_id) {
-    const ticket = await knex('tickets').where({ user_id }).limit(1).first();
-    if (!ticket) return false;
-    return { state: true, data: ticket };
-  }
+				this.eliminate(user_id, bot, result.data).then((response) => {
+					if (!response)
+						return rej(
+							new Error(`Não foi possível deletar o ticket de ${user}`)
+						);
 
-  async eliminate(user_id, bot, data) {
-    try {
-      await knex('tickets').where({ user_id }).del();
+					return res(sucess);
+				});
+			});
+		};
+	}
 
-      const canal = await bot.channels.fetch(data.channel_id);
-      canal.delete();
-      return true;
-    } catch (err) {
-      return false;
-    }
-  }
+	async eliminate(user_id, bot, data) {
+		try {
+			await knex('tickets').where({ user_id }).del();
+
+			const canal = await bot.channels.fetch(data.channel_id);
+			canal.delete();
+			return true;
+		} catch (err) {
+			return false;
+		}
+	}
 }
 
 module.exports = new TicketController();
