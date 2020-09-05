7d8c54c06c8dc7205ddc744a430afc446559b91b
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-09-05 16:06:02 -0300

    Status remove unique key

--- src/app/database/migrations/20200905142739_add_custom_functions.js
+++ src/app/database/migrations/20200905142739_add_custom_functions.js
@@ -3,7 +3,7 @@ const { onUpdateTrigger } = require('../../../../knexfile')
 exports.up = (knex) => knex.schema.createTable('presences', (table) => {
 		table.increments('id');
 		table.string('user_id', 25).unique();
-		table.string('status', 25).unique();
+		table.string('status', 25);
 
 		table.timestamp('updated_at').defaultTo(knex.fn.now());
 		table.timestamp('created_at').defaultTo(knex.fn.now());
