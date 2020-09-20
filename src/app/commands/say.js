3e78e7a24dd31b9b32c718655d2af45a1e071723
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-09-19 22:31:38 -0300

    Create command of say

---
+++ src/app/commands/say.js
@@ -0,0 +1,30 @@
+class Say {
+	constructor() {
+		this.config = {
+			name: 'say',
+			aliases: [],
+			help:
+				'Com esse comando um staffer pode falar por mim.',
+			requiredPermissions: ['MANAGE_MESSAGES'],
+		};
+		this.run = async ({ msg, args, prefix, bot }) => {
+
+			const requireMessage = await msg.reply('por favor agora digite a mensagem que você deseja que eu re-envio.')
+
+			const filterRequiringMessage = ({author}) => author.id === msg.author.id
+
+			const collectorMessageContentForReply = msg.channel.createMessageCollector(filterRequiringMessage, {
+				time: 1000 * 120,
+				max: 1,
+			})
+
+			collectorMessageContentForReply.on('collect', (messageCollected) => {
+				await requireMessage.delete()
+
+				msg.channel.send(messageCollected.content)
+			})
+		}
+	}
+}
+
+module.exports = new Say();
